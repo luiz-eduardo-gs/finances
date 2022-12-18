b4c12addab28fe4d313bceaae4cf8a2f6890c5de
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-12-18 15:36:07 -0300

    feat: migrate seed

--- migrate-seed.php
+++ migrate-seed.php
@@ -1,5 +1,5 @@
 <?php
 
-exec(__DIR__ . '/vendor/bin/phinx rollback');
+exec(__DIR__ . '/vendor/bin/phinx rollback -r');
 exec(__DIR__ . '/vendor/bin/phinx migrate');
 exec(__DIR__ . '/vendor/bin/phinx seed:run');
